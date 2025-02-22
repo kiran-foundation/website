08be18f6e22576f712a668bf05f32dd4442f7960
Anup kumar <anoopkumar0145@gamil.com>
2025-02-22 15:40:43 +0530

    header converted to tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind'; // Add this import
 
 export default defineConfig({
+  integrations: [tailwind()], // Add Tailwind integration here
   vite: {
     build: {
       rollupOptions: {
