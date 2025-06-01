0dc993a421fd95bc181cb0fa32d40818f5d9c90f
Anish Raj <anishrajdmk@gmail.com>
2025-06-01 17:07:18 +0530

    First draft of Gallery page

---
+++ src/content/config.ts
@@ -0,0 +1,14 @@
+import { defineCollection,z } from "astro:content";
+const albums = defineCollection({
+    type: "data",
+    schema: ({ image }) =>
+      z.object({
+        title: z.string(),
+        description: z.string().optional(),
+        cover: image(),
+      }),
+  });
+  
+  export const collections = {
+    albums,
+  };
