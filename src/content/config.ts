c3a0fb4cd381b3f6c0a44702e021650df2ce4529
Anish Raj <anishrajdmk@gmail.com>
2025-06-02 12:31:35 +0530

    Path fix

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,15 @@
-import { defineCollection,z } from "astro:content";
+import { defineCollection, z } from "astro:content";
+
 const albums = defineCollection({
-    type: "data",
-    schema: ({ image }) =>
-      z.object({
-        title: z.string(),
-        description: z.string().optional(),
-        cover: z.string(),
-      }),
-  });
-  
-  export const collections = {
-    albums,
-  };
+  type: "data",
+  schema: () =>
+    z.object({
+      title: z.string(),
+      description: z.string().optional(),
+      cover: z.string(), // Keep as string for file paths
+    }),
+});
+
+export const collections = {
+  albums,
+};
