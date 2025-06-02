1c62229ef4c2847dbbf98d5539c0b40eace83157
Anish Raj <anishrajdmk@gmail.com>
2025-06-02 12:53:33 +0530

    Minor fix

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,11 +2,11 @@ import { defineCollection, z } from "astro:content";
 
 const albums = defineCollection({
   type: "data",
-  schema: () =>
+  schema: ({ image }) =>
     z.object({
       title: z.string(),
       description: z.string().optional(),
-      cover: z.string(), // Keep as string for file paths
+      cover: z.string(),
     }),
 });
 
