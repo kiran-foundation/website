88bff969e86889df2a4b2d2a769f3bb49e98e08f
Anish Raj <anishrajdmk@gmail.com>
2025-06-03 18:33:16 +0530

    Gallery Page (#264)

---
+++ src/utils/albums.ts
@@ -0,0 +1,18 @@
+export async function getAlbumImages(albumId: string) {
+  const images = import.meta.glob<{ default: string }>(
+    "/public/assets/albums/**/*.{jpeg,jpg,png}",
+    { eager: true }
+  );
+
+  return Object.entries(images)
+    .filter(([path]) => path.includes(albumId))
+    .map(([path, image]) => {
+      const filename = path.split("/").pop() || "";
+      return {
+        publicPath: path.replace("/public", ""),
+        filename: filename,
+
+        src: image.default,
+      };
+    });
+}
