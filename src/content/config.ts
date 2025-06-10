52c2c7afcdaac0f331446b118ca4b33934629c16
Anish Raj <anishrajdmk@gmail.com>
2025-06-10 21:28:36 +0530

    Added the text from a md file

--- src/content/config.ts
+++ src/content/config.ts
@@ -36,9 +36,9 @@ const membershipCards = defineCollection({
     amount: z.string().optional(),
     paragraph: z.string(),
     permonth: z.string().optional(),
-    
   }),
 });
+
 export const collections = {
   albums,
   publications,
---
+++ src/content/support-form/card.md
@@ -0,0 +1,5 @@
+---
+button: "Continue the support"
+description: "Support our cause with a yearly donation of ILakh or a monthly donation of 10K."
+total: "Total Help"
+---
