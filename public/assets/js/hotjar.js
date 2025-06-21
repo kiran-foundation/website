393d69becf5c6a3857f7a1358dde61e4ad172f17
Prince Kumar Upadhyay <princeupadhyay6264@gmail.com>
2025-06-22 00:01:13 +0530

    integrating hotjar from public folder

--- public/assets/js/hotjar.js
+++ public/assets/js/hotjar.js
@@ -1,5 +1,4 @@
-
-    (function(h,o,t,j,a,r){
+(function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
         h._hjSettings={hjid:5295879,hjsv:6};
         a=o.getElementsByTagName('head')[0];
