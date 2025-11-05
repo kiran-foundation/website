4718c271e2d452386aab00fe654bb2b1455c85e6
RITI VARSHNEY <9riti2004@gmail.com>
2025-11-05 20:13:48 +0530

    Run code analyzer (#330)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,20 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind'; // Add this import
-
+import {visualizer} from 'rollup-plugin-visualizer'; // npm run build -> To view
 export default defineConfig({
   integrations: [tailwind()], // Add Tailwind integration here
   vite: {
     build: {
       rollupOptions: {
-        external: ['member']
+        external: ['member'],
+        plugins: [
+          visualizer({
+            filename: './dist/stats.html',
+            open: true,
+            gzipSize: true,
+            brotliSize: true,
+          }),
+        ],
       }
     }
   }
