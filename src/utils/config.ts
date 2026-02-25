2dcbabe4132c5eb7ba8704e5babcc534ab166d74
Anurag Jain <anurag@tatva.app>
2026-02-25 06:43:59 +0000

    Google Analytics fix

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -84,9 +84,12 @@ const CONFIG_LIVE = {
 
 const GA_TEST_ID = "G-WEK98QPZEZ";
 const GA_PROD_ID = "G-VXGS4TBSNE";
+
+export { GA_TEST_ID, GA_PROD_ID };
+
 // Select configuration based on environment
 const SUPPORT_US_CONFIG = isProduction() ? CONFIG_LIVE : CONFIG_TEST;
 
 const GA_TRACKING_ID = isProduction() ? GA_PROD_ID : GA_TEST_ID;
 // Export the selected configuration and utility functions
-export { SUPPORT_US_CONFIG, isProduction,GA_TRACKING_ID};
+export { SUPPORT_US_CONFIG, isProduction, GA_TRACKING_ID };
